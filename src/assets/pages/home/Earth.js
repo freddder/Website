f9ab4f0f31336724741cd8f7feddee0a5345d7c0
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2023-12-29 20:04:05 -0500

    Land complete

--- src/assets/pages/home/Earth.js
+++ src/assets/pages/home/Earth.js
@@ -5,18 +5,32 @@ import gsap from 'gsap';
 function Earth() {
 
   useEffect(() => {
-    //gsap.to(".bonezone", {
-    //  rotation: 360,
-    //  duration: 3,
-    //  repeat: -1,
-    //  ease: 'none'
-    //});
+    var continent1 = gsap.timeline({ repeat: -1 });
+    continent1.to('.land-1', { x: 1500, duration: 8, ease: 'none' });
+    continent1.to('.land-2', { x: 1500, duration: 8, ease: 'none' }, "<");
+    
+    var continent2 = gsap.timeline({ repeat: -1 });
+    continent2.to('.land-3', { x: 750, duration: 4, ease: 'none' });
+    continent2.to('.land-3', { x: -750,  duration: 0, ease: 'none' });
+    continent2.to('.land-3', { x: 0,  duration: 4, ease: 'none' });
+    
+    var continent3 = gsap.timeline({ repeat: -1 });
+    continent3.to('.land-4', { x: 750, duration: 4, ease: 'none' });
+    continent3.to('.land-4', { x: -750,  duration: 0, ease: 'none' });
+    continent3.to('.land-4', { x: 0,  duration: 4, ease: 'none' });
+
+    var continent4 = gsap.timeline({ repeat: -1 });
+    continent4.to('.land-5', { x: 1500, duration: 8, ease: 'none' });
   }, [])
 
   return (
     <div className='container'>
       <div className="ocean">
-
+          <div className='land-1' style={{ top: '300px', left: '-400px' }} />
+          <div className='land-2' style={{ top: '400px', left: '-450px' }} />
+          <div className='land-3' style={{ top: '330px', left: '200px' }} />
+          <div className='land-4' style={{ top: '100px', left: '0px' }} />
+          <div className='land-5' style={{ top: '200px', left: '-850px' }} />
       </div>
     </div>
   );
