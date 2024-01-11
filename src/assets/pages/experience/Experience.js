cc814e56c137feafd39686a76b805ed9943bbad8
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2024-01-11 13:39:24 -0500

    Finished Experience page (maybe)

--- src/assets/pages/experience/Experience.js
+++ src/assets/pages/experience/Experience.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import Project from './Project';
 import './Experience.css';
+import path from '../../images/Path_of_Titans_Banner.jpg';
+import pkm from '../../images/PKM_Banner.png';
+import unannounced from '../../images/Unannounced_Banner.png';
 
 const projects = [
   {
@@ -10,8 +13,8 @@ const projects = [
     link: 'https://pathoftitans.com/',
     responsabilities: ['Bug Fixing', 'Feature Planning', 'Quality Assurance'],
     skills: ['C++', 'Unreal Engine 5'],
-    imgSrc: '../../images/Path_of_Titans_Banner.jpg',
-    imgAlt: '',
+    imgSrc: path,
+    imgAlt: 'Path of Titans Banner',
   },
   {
     projName: 'Unannounced Unreal Game',
@@ -19,8 +22,8 @@ const projects = [
     link: '',
     responsabilities: ['Prototyping', 'Feature Development', 'Animation Programming'],
     skills: ['C++', 'Unreal Engine 5'],
-    imgSrc: '',
-    imgAlt: '',
+    imgSrc: unannounced,
+    imgAlt: 'Unannounced Game Banner',
   },
   {
     projName: '[PERSONAL PROJECT] OpenGL Pokémon Game',
@@ -28,17 +31,8 @@ const projects = [
     link: 'https://github.com/freddder/NewEngine',
     responsabilities: ['Engine Programming', 'Graphics Programming', 'Feature Planning/Development'],
     skills: ['C++', 'OpenGL'],
-    imgSrc: '',
-    imgAlt: '',
-  },
-  {
-    projName: '[PERSONAL PROJECT] OpenGL Pokémon Game',
-    title: 'Solo Developer',
-    link: 'https://github.com/freddder/NewEngine',
-    responsabilities: ['Engine Programming', 'Graphics Programming', 'Feature Planning/Development'],
-    skills: ['C++', 'OpenGL'],
-    imgSrc: '',
-    imgAlt: '',
+    imgSrc: pkm,
+    imgAlt: 'Pokemon OpenGL Game Banner',
   },
 ];
 
@@ -52,11 +46,13 @@ function Experience() {
   }, [])
 
   return (
-    <div style={{ minHeight: '90vh', width: '80vw', margin: '0 10vw 0 10vw' }}>
-      { projects.map((projProps, index) => {
-        projProps['projNumber'] = index + 1;
-        return <Project {...projProps}/>
-      })}
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ minHeight: '90vh', width: '80vw', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: '30px 30px 0px 30px' }}>
+        { projects.map((projProps, index) => {
+          projProps['projNumber'] = index + 1;
+          return <Project {...projProps}/>
+        })}
+      </div>
     </div>
   );
 }
--- src/assets/pages/experience/Project.js
+++ src/assets/pages/experience/Project.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import path from '../../images/Path_of_Titans_Banner.jpg'
 
 function Project(props) {
 
     return (
         <div className={`project-${props.projNumber}`}>
-            <img src={path} alt={props.imgAlt} style={{ maxWidth: '30%', marginRight: '20px', objectFit: 'contain' }}/>
+            <img src={props.imgSrc} alt={props.imgAlt} style={{ maxWidth: '30%', marginRight: '20px', objectFit: 'contain' }}/>
             <div style={{ textAlign: 'left' }}>
                 { props.link !== '' 
                     ? <h1><a href={props.link} target="_blank" rel="noreferrer" style={{ color: 'white' }}>{props.projName}</a></h1>
