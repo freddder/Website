d811b36c13da9c3b73b936910a4f3136dc6e34cc
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2024-08-25 18:24:29 -0400

    Updated about page with more details

--- src/assets/pages/About/About.js
+++ src/assets/pages/About/About.js
@@ -1,42 +1,60 @@
 import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import './About.css';
-import Picture from '../../images/My_Picture.jpg';
+//import Picture from '../../images/My_Picture.jpg';
 
 function About() {
 
   useEffect(() => {
     var tl = gsap.timeline();
     tl.fromTo(".textBlock", { y: 50, opacity: 0 }, { y: 0, opacity: 1, duration: 1 });
-    tl.fromTo("li", { y: -20, opacity: 0 }, { y: 0, opacity: 1, duration: 1 }, "-=0.4");
+    tl.fromTo("iframe", { y: 50, opacity: 0 }, { y: 0, opacity: 1, duration: 1 }, "-=0.4");
   }, [])
 
   return (
     <div style={{ minHeight: '90vh' }}>
       <div className='textBlock'>
-        <img src={Picture} alt='Lucas portrait' className='my-pic'/>
+        {/*<img src={Picture} alt='Lucas portrait' className='my-pic'/>*/}
         <p>
-          I've graduated from a program that taught me about most (if not all) different fields of programming, but the most important thing
-          I learned is that I love working with technology. Either that being implementing new features or building something from ground up.
-          No matter what it is, my satisfaction comes from seeing my work in action and learning new skills from projects and teammates.
+        I've had a passion for technology since I was a kid, but specially for games. Since learnign how to code, I always try to learn new skills and 
+        implement them on personal projects. From special graphic effects to AI, there is nothing I'm not willing to learn. Same goes for software 
+        developement.
         </p>
         <p style={{margin: '20px 0px 20px 0px'}}>
-          Here are some skills that I've aquired while working on different projects:
+          Here are some skills, technologies and languages I learned from both professional and personal projects:
         </p>
-        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly', margin: '20px 0px 20px 0px'}}>
+          <div>
+            <li>C</li>
+            <li>C++</li>
+            <li>C#</li>
+            <li>OpenGL</li>
+          </div>
           <div>
             <li>React</li>
             <li>TypeScript</li>
             <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS/SASS</li>
+          </div>
+          <div>
+            <li>React</li>
+            <li>Jira</li>
             <li>SQL</li>
+            <li>NoSQL</li>
             <li>AWS</li>
           </div>
           <div>
-            <li>C#</li>
-            <li>C++</li>
-            <li>OpenGL</li>
-            <li>Unreal Engine 5</li>
+            <li>Unreal Engine</li>
+            <li>Unity</li>
+            <li>Godot</li>
           </div>
+        </div>
+        <p>In case you want to a taste of my work, here is a demo video of my biggest project and (now most) of it's features:</p>
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0px 20px 0px' }}>
+          <iframe height="300" width="500" title='Personal Project Demo Video'
+            src="https://www.youtube.com/embed/1azS6HWtYpU"> 
+          </iframe>
         </div>
       </div>
     </div>
