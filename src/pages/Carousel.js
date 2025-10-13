f7760d60aff2b910c3d58a8ba648643018678750
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2025-10-12 20:47:31 -0400

    Added carousel component

---
+++ src/pages/Carousel.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './Carousel.css'
+
+import arrow from '../assets/images/Arrow.png';
+
+function Carousel(props) {
+    const [slide, setSlide] = useState(0);
+
+    const nextSlide = () => {
+        setSlide(slide === props.slides.length - 1 ? 0 : slide + 1);
+    }
+
+    const previousSlide = () => {
+        setSlide(slide === 0 ? props.slides.length - 1 : slide - 1);
+    }
+
+    return (
+        <div className='carousel'>
+            <img src={arrow} className='arrow arrow-left' alt='left arrow' onClick={previousSlide}/>
+            {props.slides.map((slideData, idx) => {
+                return <img key={idx} src={slideData.background} alt='' className={slide === idx ? 'slide' : 'slide slide-hidden'}/>
+                
+            })}
+            <img src={arrow} className='arrow arrow-right' alt='right arrow' onClick={nextSlide}/>
+            <span className='indicatorsContainer'>
+                {props.slides.map((_, idx) => {
+                    return <button key={idx} onClick={() => setSlide(idx)} className={slide === idx ? 'indicator' : 'indicator indicator-inactive'}/>
+                })}
+            </span>
+        </div>
+    );
+}
+
+export default Carousel;
