bd0d5dbbab175caab1a58e37245bb50806d73af4
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2025-10-27 21:26:28 -0400

    Trying hashrouter instead of browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/home/Home';
 import Experience from './pages/experience/Experience';
